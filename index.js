3def2cdd8272e13a98e4406efe2972ffe41d4591
rasheedshina22 <rasheedshinaopeyemi@gmial.com>
2019-07-09 06:32:43 -0700

    removed modules functionality

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const search = require("./redditApi");
+
+const search = (searchTerm, sortBy, searchLimit) => {
+  return (
+    fetch(
+      `https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}$limit=${searchLimit}`
+    )
+      .then(res => res.json())
+      //since what we want is the data object, we can map thru it
+      .then(data => data.data.children.map(data => data.data))
+      .catch(err => console.log(err))
+  );
+};
+
 
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
